f28f36136b34a67715c7e9202c3e4e3a59f06742
onomoh1 <onomohfatimah@gmail.com>
2026-06-07 08:49:24 +0100

    feat: add kubernetes manifests, github actions ci/cd pipeline

--- terraform/root/main.tf
+++ terraform/root/main.tf
@@ -35,7 +35,7 @@ module "rds_postgres" {
   engine                     = "postgres"
   engine_version             = "15"
   db_name                    = "retailstore"
-  username                   = "admin"
+  username                   = "dbadmin"
   password                   = var.postgres_password
   port                       = 5432
   vpc_id                     = module.vpc.vpc_id
